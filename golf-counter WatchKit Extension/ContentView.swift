233618287a8cd85498355ef90f2d8db4ca6a374f
higaki-takanori <garoumare@gmail.com>
2022-02-04 20:01:26 +0900

    Initial Commit

---
+++ golf-counter WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  golf-counter WatchKit Extension
+//
+//  Created by 檜垣貴仁 on 2022/02/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ golf-counter WatchKit Extension/golf_counterApp.swift
@@ -0,0 +1,19 @@
+//
+//  golf_counterApp.swift
+//  golf-counter WatchKit Extension
+//
+//  Created by 檜垣貴仁 on 2022/02/04.
+//
+
+import SwiftUI
+
+@main
+struct golf_counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
